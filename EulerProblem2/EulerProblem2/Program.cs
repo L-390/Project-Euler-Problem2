07b9078d9251e8ea950e78a786ae65273a6ec2c8
Liam <67001127+L-390@users.noreply.github.com>
2020-07-06 09:54:58 +0200

    Add files via upload

---
+++ EulerProblem2/EulerProblem2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace EulerProblem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 1;
+            int number2 = 1;
+            int sum = 0;
+            int i = 0;
+
+            while(i == 0)
+            {
+                number1 = number1 += number2;
+
+                number2 = number2 += number1;
+                
+                if(number1 <= 4000000 && number2 <= 4000000)
+                {
+                    Console.WriteLine(number1);
+                    Console.WriteLine(number2);
+
+                    if ((number1 % 2) == 0)
+                    {
+                        sum += number1;   
+                    }
+
+                    if ((number2 % 2) == 0)
+                    {       
+                        sum += number2;
+                    }
+                }
+                else
+                {
+                    i = 1;
+                }
+            }
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
